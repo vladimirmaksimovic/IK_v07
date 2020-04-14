637af6dcac245d704071dde853212cc4963fa197
itobuka <itobuka2019-10@ftn.edu.rs>
2020-04-14 17:14:00 +0200

    sredjujem kod

--- controller/login.php
+++ controller/login.php
@@ -13,6 +13,7 @@ switch ($action) {
     if (!empty($_POST)) {
       $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
       $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+
       foreach (array_reverse($user) as $value) {
         if ($username == $value['name'] && $password == $value['password']) {
           $_SESSION['ses'] = $username;
--- views/footer.php
+++ views/footer.php
@@ -1,141 +1,73 @@
 <!-- Footer Section -->
-
 <footer>
-
   <div class="container">
-
     <div class="row p-4">
-
       <div class="col-md-4 col-sm-12">
-
         <h3>Mapa sajta</h3>
-
         <ul>
-
           <li>
-
             <a href="index.php"><i class="fa fa-home"></i> Početna</a>
-
           </li>
-
           <li>
-
             <a href="events.php"><i class="fa fa-bicycle" aria-hidden="true"></i>
-
               Aktivnosti</a>
-
           </li>
-
           <li>
-
             <a href="workshop.php" class="workshop-title"><i class="fa fa-paint-brush" aria-hidden="true"></i>
-
               Kreativna jazbina</a>
-
           </li>
-
           <li>
-
             <a href="gallery.php"><i class="fa fa-picture-o" aria-hidden="true"></i>
-
               Galerija</a>
-
           </li>
-
           <li>
-
             <a href="about.php"><i class="fa fa-address-card-o" aria-hidden="true"></i> O
-
               meni</a>
-
           </li>
-
         </ul>
-
       </div>
-
       <div class="col-md-4 col-sm-12" id="contact_bg">
-
         <h3>Kontakt</h3>
-
         <ul>
-
           <li>Adresa:</li>
-
           <li>
-
             <i class="fa fa-map-marker" aria-hidden="true"></i> Ljubić kej
-
             19/13, 32000 Čačak, Srbija
-
           </li>
-
           <li>Telefon:</li>
-
           <li>
-
             <i class="fa fa-mobile" aria-hidden="true"></i> +381 (0)64 23 23
-
             178
-
           </li>
-
           <li>Mail:</li>
-
           <li>
-
             <i class="fa fa-envelope" aria-hidden="true"></i>
-
             <a href="mailto: ivanaknezevic44@gmail.com">ivanaknezevic44@gmail.com</a>
-
           </li>
-
         </ul>
-
       </div>
-
       <div class="col-md-4 col-sm-12 text-center pt-1">
-
         <a href="https://twitter.com/" class="col social-media" target="_blank"><i class="fa fa-twitter-square fa-3x"></i></a>
-
         <a href="https://www.google.com/intl/sr/gmail/about/" target="_blank" class="col social-media"><i class="fa fa-envelope fa-3x"></i></a>
-
         <div class="w-100 social-media"></div>
-
         <a href="https://www.facebook.com/" target="_blank" class="col social-media"><i class="fa fa-facebook-square fa-3x"></i></a>
-
         <a href="https://www.youtube.com/" class="col social-media" target="_blank"><i class="fa fa-youtube fa-3x"></i></a>
-
       </div>
-
     </div>
-
   </div>
-
   <div class="row p-3 m-0">
-
     <button type="button" onclick="backToTheFuture()" class="btn btn-outline-light text-light rounded-pill ml-auto to-the-top">
-
       Vrh strane
-
     </button>
-
   </div>
-
   <div class="copyright text-center bg-dark text-light w-100">
-
     &#169;2019 | Design <span class="extra-space"></span><a href="admin.php">GMVP Solution</a>
-
   </div>
-
 </footer>
-
 <!-- /Footer Section -->
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </body>
