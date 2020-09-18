8eadd0672ca64257ea1309db6a0c9402dbaa5750
itobuka <itobuka2019-10@ftn.edu.rs>
2020-09-18 10:26:06 +0200

    fixing missing link in header

--- view/view-sr/header.php
+++ view/view-sr/header.php
@@ -9,7 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <script src="../../assets/js/jquery.js"></script>
-  <link rel="stylesheet" href="../../css/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="../../assets/css/bootstrap/bootstrap.min.css" />
   <script src="../../assets/js/bootstrap/bootstrap.bundle.min.js"></script>
   <script src="../../assets/js/active.js"></script>
   <script src="../../assets/js/main.js"></script>
@@ -18,7 +18,7 @@
   <!-- <link rel="stylesheet" id="resonant-google-fonts-css"
     href="https://fonts.googleapis.com/css?family=Overpass:400,400i,600,600i,700,700i&amp;subset=latin%2Clatin-ext"
     type="text/css" media="all" /> -->
-  <title>Atelje</title>
+  <title>Atelje Umetnika</title>
 </head>
 
 <body>
